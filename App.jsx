c1d9ed9016592e1e22604ed6ccdb050567378b66
harun_1515 <132686599+harun1155@users.noreply.github.com>
2026-08-28 18:49:19 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import StudentCard from "./components/Studentcard";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+
+      <StudentCard />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
